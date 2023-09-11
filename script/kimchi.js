d9fed74308867ac4e4498e1c993afd2328037e0f
taewookim111 <taewookim000p@gmail.com>
2023-09-11 11:25:13 +0900

    한식포트폴리오

---
+++ script/kimchi.js
@@ -0,0 +1,121 @@
+const panel2 = document.querySelector(".panel2");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const lis = panel2.querySelectorAll("li");
+const map = document.querySelector("#map")
+const div = map.querySelectorAll("div");
+const leftDiv = map.querySelector(".left");
+const rightDiv = map.querySelector(".right");
+const leftDiv1 = map.querySelector(".left1");
+const rightDiv1 = map.querySelector(".right1");
+const leftDiv2 = map.querySelector(".left2");
+const chilli = document.querySelector(".banner h2");
+const bg2 = document.querySelector(".bg2");
+const bg3 = document.querySelector(".bg3");
+console.log(leftDiv);
+let posArr = [];
+let len = lis.length;
+let lens = div.length;
+console.log(lens);
+enableClick = true;
+
+
+init();
+
+prev.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (enableClick) {
+        prevSlide();
+        enableClick = false;
+    }
+})
+
+next.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (enableClick) {
+        nextSlide();
+        enableClick = false;
+    }
+})
+
+for(let el of div){
+    posArr.push(el.offsetTop);
+}
+posArr.push(div[lens - 1].offsetTop + div[lens - 1].offsetHeight);
+let pos_Arr = Array.from(posArr);
+console.log(pos_Arr); //[1700, 2500, 3400, 4300, 5200, 6040]
+window.addEventListener("scroll", ()=>{
+    let scroll = window.scrollY || window.pageYOffset
+    || document.documentElement.scrollTop;
+    console.log(scroll);
+
+    let bgscroll = scroll - 300;
+    let opa = (0 + bgscroll / 1000).toString();
+    bg2.style.opacity = opa;
+    let bgscroll2 = scroll - 5400;
+    let opac = (0 + bgscroll2 / 1000).toString();
+    bg3.style.opacity = opac;
+    
+
+    if(scroll >= pos_Arr[0] - 700 && scroll <= pos_Arr[0] - 104){
+        leftDiv.style.left = `${scroll - pos_Arr[0] + 140}px`;
+    };
+
+    if(scroll >= pos_Arr[0] - 100 && scroll <= pos_Arr[1] - 125){
+        rightDiv.style.right = `${scroll - pos_Arr[1] + 180}px`;
+    };
+
+    if(scroll >= pos_Arr[1] - 700 && scroll <= pos_Arr[2] - 90){
+        leftDiv1.style.left = `${scroll - pos_Arr[2] + 140}px`;
+    };
+    
+    if(scroll >= pos_Arr[2] + 300 && scroll <= pos_Arr[3] - 300){
+        rightDiv1.style.right = `${scroll - pos_Arr[3] + 360}px`;
+    };
+
+    if(scroll >= pos_Arr[3] + 400 && scroll <= pos_Arr[4] - 200){
+        leftDiv2.style.left = `${scroll - pos_Arr[4] + 270}px`;
+    };
+
+    if(scroll >= 0 && scroll <= pos_Arr[0] - 950){
+        chilli.style.left = `${scroll - 452}px`;
+    };
+});
+
+
+function prevSlide() {
+    new Anim(panel2, {
+        prop: "left",
+        value: "0%",
+        duration: 800,
+        callback: () => {
+            panel2.style.left = "-100%";
+            panel2.prepend(panel2.lastElementChild);
+            enableClick = true;
+        }
+    })
+}
+
+function nextSlide() {
+    new Anim(panel2, {
+        prop: "left",
+        value: "-200%",
+        duration: 800,
+        callback: () => {
+            panel2.append(panel2.firstElementChild);
+            panel2.style.left = "-100%";
+            enableClick = true;
+        }
+    });
+}
+
+function init(){
+    panel2.style.width = `${100 * len}%`;
+    //ul의 너비값을 li의 갯수를 js가 세어서 자동 계산하는 코드
+    lis.forEach((index) => {
+    //각 li의 너비값을 자동으로 계산하는 코드
+    index.style.width = `${100 / len}%`;
+    })
+    panel2.style.left = "-100%";
+    panel2.prepend(panel2.lastElementChild);
+    }
